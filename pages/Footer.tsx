a29345f65b8c0d38f65d0d13af662e302f4462b7
Anto Pinjatic <34194750+Corpano@users.noreply.github.com>
2023-03-19 01:27:59 -0700

    quick fix

--- pages/Footer.tsx
+++ pages/Footer.tsx
@@ -1,3 +1,4 @@
+import Link from 'next/link'
 import styles from '../styles/footer.module.css'
 
 export default function Footer(){
@@ -11,7 +12,7 @@ export default function Footer(){
         <div className={styles['two-sides_container']}>
             <div className={styles['left']}>
                 <h3>Important Notice</h3>
-                <p>Regarding the quizzes on this website, I can assure you that the scores are not a reflection of your intelligence or abilities in any way. They are purely for entertainment purposes and to demonstrate the capabilities of AI in generating engaging and interactive content. It's important to always take the results of any online quiz with a grain of salt and not take them too seriously.Regarding the quizzes on this website, I can assure you that the scores are not a reflection of your intelligence or abilities in any way. They are purely for entertainment purposes and to demonstrate the capabilities of AI in generating engaging and interactive content. It's important to always take the results of any online quiz with a grain of salt and not take them too seriously.<br/><br/>Written by Chat GPT</p>
+                <p>Regarding the quizzes on this website, I can assure you that the scores are not a reflection of your intelligence or abilities in any way. They are purely for entertainment purposes and to demonstrate the capabilities of AI in generating engaging and interactive content. It&apos;s important to always take the results of any online quiz with a grain of salt and not take them too seriously.Regarding the quizzes on this website, I can assure you that the scores are not a reflection of your intelligence or abilities in any way. They are purely for entertainment purposes and to demonstrate the capabilities of AI in generating engaging and interactive content. It&apos;s important to always take the results of any online quiz with a grain of salt and not take them too seriously.<br/><br/>Written by Chat GPT</p>
             </div>
 
             <div className={styles['right']}>
@@ -19,7 +20,7 @@ export default function Footer(){
                 <p>If you are interested to learn more about AI or would like to chat, feel free to contact me. I am looking to meet more like minded individuals interested in AI, send me a message!</p>
 
                 <button className={styles['contact-me']}>
-                    <a href="/" className={styles["contact-me_text"]}>CONTACT ME</a>
+                    <Link href="/" className={styles["contact-me_text"]}>CONTACT ME</Link>
                 </button>
             </div>
         </div>
